8968aa6dd9351b9a4613aefb5c1eb95ad4309a84
taoqf <tao_qiufeng@126.com>
2020-03-26 16:55:56 +0800

    v1

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-import am1 from '@mmstudio/am000001';
 import am2 from '@mmstudio/am000002';
 
 /**
  * 初始化网络连接
  */
-export default function init(mm: am1, base_url: string) {
-	am2(mm, 'host', base_url);
+export default function init(base_url: string) {
+	am2('host', base_url);
 }
